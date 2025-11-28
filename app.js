d7a7b479e86abe4dfcf129e40923bc5b36856b6a
Yunus Emre ASLAN <unuvitec@gmail.com>
2025-11-28 20:49:55 +0300

    beginning

---
+++ app.js
@@ -0,0 +1,10 @@
+function add(n1, n2) {
+    console.log(typeof number1);
+    return n1 + n2;
+}
+;
+var number1 = 4;
+var number2 = 5;
+var result = add(number1, number2);
+console.log(result);
+console.log(number1);
---
+++ app.ts
@@ -0,0 +1,12 @@
+
+function add(n1:number, n2:number){
+    console.log(typeof number1);
+    return n1 + n2;
+};
+
+const number1 = 4;
+const number2 = 5;
+
+const result = add(number1,number2);
+console.log(result);
+console.log(number1);
